66caeb7b2d03510ae59dfd26b760e7971396f1c6
Nacho Pérez Carretero <ignacio.perez@udarealestate.com>
2021-03-14 18:48:48 +0100

    Create basic happy path flow without iteration

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
+BATCH_SIZE = 10
+
+def get_batch_from_id(_id=0):
+    print("Query DB for id > _id and LIMIT BATCH_SIZE")
+    return [i for i in range(BATCH_SIZE)]
+
+def copy_batch(batch):
+    for migration in batch:
+        print("Copy item: {item}".format(item=migration))
+
+def update_path(batch):
+    print("Transaction updating {items} rows".format(items=len(batch)))
+
+def delete_batch(batch):
+    for migration in batch:
+        print("Testing item {item}".format(item=migration))
+        print("Deleting item {item}".format(item=migration))
+
 def run():
-    print("running")
+    # Get first batch
+    batch = get_batch_from_id()
+    # If batch size is larger than 0
+    if len(batch) > 0:
+        copy_batch(batch)
+        update_path(batch)
+        delete_batch(batch)
+    # next batch
 
 
 if __name__ == '__main__':
