f859e4f8a1bb4a96fd27410315b0b11ed66a80ac
Nacho Pérez Carretero <ignacio.perez@udarealestate.com>
2021-03-16 14:20:53 +0100

    Use Job instead of tuples and add new_key with path

--- main.py
+++ main.py
@@ -1,30 +1,47 @@
 from src.main_db import MainDB
 from src.aws_helper import AWSHelper
 from dotenv import load_dotenv
+from dataclasses import dataclass
 
 BATCH_SIZE = 10
 OLD_PATH_PREFIX = "images"
 NEW_PATH_PREFIX = "avatar"
+@dataclass
+class Job():
+    job_id: int
+    old_key: str
+    new_key: str
+
+def generate_batch_from_raw(jobs_raw):
+    batch = []
+    for job in jobs_raw:
+        batch.append(Job(
+            job[0],
+            job[1],
+            job[1].replace(
+                OLD_PATH_PREFIX,
+                NEW_PATH_PREFIX
+            )
+        ))
+    return batch
 
 def get_batch_from_id(_id=0):
-    batch = MainDB().get_batch_from_id_with_prefix(
+    jobs_raw = MainDB().get_batch_from_id_with_prefix(
         batch_size=BATCH_SIZE,
         _id=_id,
         prefix=OLD_PATH_PREFIX
     )
+    batch = generate_batch_from_raw(jobs_raw)
     print(batch)
     return batch
 
 def migrate_batch(batch):
     helper = AWSHelper()
-    for migration in batch:
-        source_key = migration[1]
-        destination_key = migration[1].replace(
-            OLD_PATH_PREFIX,
-            NEW_PATH_PREFIX
-        )
+    for job in batch:
+        source_key = job.old_key
+        destination_key = job.new_key
+        print("Copy item: {item}".format(item=job))
         helper.copy_key(source_key, destination_key)
-        print("Copy item: {item}".format(item=migration))
 
 def update_path(batch):
     print("Transaction updating {items} rows".format(items=len(batch)))
